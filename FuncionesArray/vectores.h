87006fa3e8712cb102d48ae1ea500fa41ccc48e8
Usuario <Usuario@DESKTOP-S2L6J8C>
2023-11-09 14:00:16 +0100

    relacion de vectores hasta el 2

---
+++ FuncionesArray/vectores.h
@@ -0,0 +1,37 @@
+/*
+ * vectores.h
+ *
+ *  Created on: 9 nov 2023
+ *      Author: Usuario
+ */
+
+#ifndef VECTORES_H_
+#define VECTORES_H_
+	/**
+	 * Procedimiento que pide un vector por teclado
+	 * Pide tantos elementos como indique el parámetro tam
+	 */
+	void pedirVector(int tam, int vector[]);
+
+	/**
+	 * Procedimineto que muestra un vector por pantalla. Hay que pasarle
+	 * el argumento tam para indicar cuantos elementos del vector deben
+	 * mostrarse.
+	 */
+	void mostrarVector(int tam, int vector[]);
+
+	void mostarVectorCorchete(int tam, int vector[]);
+
+	/**
+	 * Función que devuelve la casilla de la primera ocurrencia en el vector
+	 * del número indicado por buscar. Devuelve -1 si el elemento no ha sido
+	 * encontrado
+	 */
+	int posicionPrimerElemento(int tam, int vector[], int buscar);
+
+	/**
+	 * Mostrar elementos menores que el número dado por pantalla
+	 */
+	void mostrarMenoresQue(int tam, int vector[], int num);
+
+#endif /* VECTORES_H_ */
